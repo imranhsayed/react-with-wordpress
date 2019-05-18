94054fdcbd5ab581eb27e5cf08190f97abebaeee
Imran Sayed <imranhsayed@gmail.com>
2019-05-18 17:03:02 +0530

    Create single post component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Router } from "@reach/router";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
+import SinglePost from "./components/SinglePost";
 
 class App extends React.Component {
 
@@ -13,6 +14,7 @@ class App extends React.Component {
 				<Home path="/"/>
 				<Login path="/login"/>
 				<Dashboard path="/dashboard/:userName"/>
+				<SinglePost path="/post/:id"/>
 			</Router>
 		);
 	}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,74 @@
 import React from 'react';
 import Navbar from "./Navbar";
+import axios from 'axios';
+import Loader from "../loader.gif";
+import renderHTML from 'react-render-html';
+import Moment from 'react-moment';
+import { Link } from '@reach/router';
 
 class Home extends React.Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			loading : false,
+			posts: [],
+			error: ''
+		};
+	}
+
+	createMarkup = ( data ) => ({
+		__html: data
+	});
+
+	componentDidMount() {
+		const wordPressSiteURL = 'http://localhost:8888/wordpress';
+		this.setState( { loading: true }, () => {
+			axios.get( `${wordPressSiteURL}/wp-json/wp/v2/posts/` )
+				.then( res => {
+					if ( res.data.length ) {
+						this.setState( { loading: false, posts: res.data } );
+					} else {
+						this.setState( { loading: false, error: 'No Posts Found' } );
+					}
+				} )
+				.catch( err => this.setState( { loading: false, error: err.response.data.message } ) );
+		} )
+	}
+
 	render() {
+
+		const { loading, posts, error } = this.state;
+
 		return(
+			<React.Fragment>
 			<Navbar/>
+				{ error && <div className="alert alert-danger" dangerouslySetInnerHTML={ this.createMarkup( error ) }/> }
+				{ posts.length ? (
+					<div className="mt-5 posts-container">
+						{ posts.map( post => (
+							<div key={post.id} className="card border-dark mb-3" style={{maxWidth: '50rem'}}>
+								<div className="card-header">
+									<Link to={`/post/${post.id}`} className="text-secondary font-weight-bold" style={{ textDecoration: 'none' }}>
+										{renderHTML( post.title.rendered )}
+									</Link>
+								</div>
+								<div className="card-body">
+									<div className="card-text post-content">{ renderHTML( post.excerpt.rendered ) }</div>
+								</div>
+								<div className="card-footer">
+									<Moment fromNow >{post.date}</Moment>
+									<Link to={`/post/${post.id}`} className="btn btn-secondary float-right" style={{ textDecoration: 'none' }}>
+										Read More...
+									</Link>
+								</div>
+							</div>
+						) ) }
+					</div>
+				) : '' }
+				{ loading && <img className="loader" src={Loader} alt="Loader"/> }
+			</React.Fragment>
 		);
 	}
 }

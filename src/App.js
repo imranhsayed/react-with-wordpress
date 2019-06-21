2839167402a3b92584c3d7a70fc940ec0988c235
Imran Sayed <imranhsayed@gmail.com>
2019-06-21 21:15:48 +0530

    Add react-boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './style.css';
-import { Router } from "@reach/router";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import Home from "./components/Home";
-import SinglePost from "./components/SinglePost";
 
 class App extends React.Component {
 
 	render() {
 		return (
-			<Router>
-				<Home path="/"/>
-				<Login path="/login"/>
-				<Dashboard path="/dashboard/:userName"/>
-				<SinglePost path="/post/:id"/>
-			</Router>
+			<div>
+				This is App Component.
+			</div>
 		);
 	}
 }
--- src/components/functions.js
+++
@@ -1,7 +0,0 @@
-export const isLoggedIn = () => {
-	return localStorage.getItem( 'token' );
-};
-
-export const getUserName = () => (
-	localStorage.getItem( 'userName' )
-);

ded6793dbec74e963b84d0a94eaf8d8606017461
Imran Sayed <imranhsayed@gmail.com>
2019-11-12 21:52:30 +0530

    Create blog component

---
+++ src/components/Blogs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./Navbar";
+
+const Blogs = () => {
+
+	return (
+		<React.Fragment>
+			<Navbar/>
+			<div>content</div>
+		</React.Fragment>
+	)
+};
+
+export default Blogs;
